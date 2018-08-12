f48cdfa5d5a0f7878f6ec35a24246b39fdb18571
Magomed Makhmudov <mmriadovich@gmail.com>
2018-08-12 20:20:46 +0300

    change some system files

--- guestbook/guestbookapp/views.py
+++ guestbook/guestbookapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'guestbookapp/index.html')
+
+
+def sign(request):
+    return render(request, 'guestbookapp/sign.html')
