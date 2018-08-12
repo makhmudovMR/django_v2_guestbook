24edf23326fd9f4f12e671e234553248faa835ff
Magomed Makhmudov <mmriadovich@gmail.com>
2018-08-13 00:37:10 +0300

    guestbookapp files

--- guestbook/guestbookapp/models.py
+++ guestbook/guestbookapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Comment(models.Model):
+
+    name = models.CharField(max_length=50)
+    comment = models.TextField()
+    date_added = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return str(self.name)
+
+    class Meta:
+
+        verbose_name = 'Comment'
+        verbose_name_plural = 'Comments'
--- guestbook/guestbookapp/views.py
+++ guestbook/guestbookapp/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Comment
+from .forms import CommentsForm
 
 def index(request):
-    return render(request, 'guestbookapp/index.html')
+    comments = Comment.objects.order_by('-date_added')
+    context = {'comments': comments}
+    return render(request, 'guestbookapp/index.html', context)
 
 
 def sign(request):
-    return render(request, 'guestbookapp/sign.html')
+    if request.method == 'POST':
+        form = CommentsForm(request.POST)
+
+        if form.is_valid(): # if token is valid
+            new_comment = Comment(name=request.POST['name'], comment=request.POST['comment'])
+            new_comment.save()
+            return redirect('index')
+    else:
+        form = CommentsForm()
+
+    context = {'form': form}
+    return render(request, 'guestbookapp/sign.html', context=context)
